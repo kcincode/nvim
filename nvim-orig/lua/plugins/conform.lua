92c1783ba815111235717abfa17d046e9a0daaff
Nick Felicelli <nicholas.felicelli@cchmc.org>
2024-07-09 11:27:34 -0400

    Updating configs

---
+++ nvim-orig/lua/plugins/conform.lua
@@ -0,0 +1,47 @@
+return { -- Autoformat
+	"stevearc/conform.nvim",
+	lazy = false,
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "[C]ode [F]ormat buffer",
+		},
+	},
+	opts = {
+		notify_on_error = true,
+		format_on_save = function(bufnr)
+			-- Disable "format_on_save lsp_fallback" for languages that don't
+			-- have a well standardized coding style. You can add additional
+			-- languages here or re-enable it for the disabled ones.
+			local disable_filetypes = { c = true, cpp = true }
+			return {
+				timeout_ms = 5000,
+				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+			}
+		end,
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "ruff_lsp" },
+			htmldjango = { "djlint" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			json = { "prettier" },
+			htmldjango = { "djlint" },
+
+			-- Conform can also run multiple formatters sequentially
+			-- python = { "isort", "black" },
+			--
+			-- You can use a sub-list to tell conform to run *until* a formatter
+			-- is found.
+			-- javascript = { { "prettierd", "prettier" } },
+		},
+	},
+}
---
+++ nvim-orig/lua/plugins/which-key.lua
@@ -0,0 +1,24 @@
+return { -- Useful plugin to show you pending keybinds.
+	"folke/which-key.nvim",
+	event = "VimEnter", -- Sets the loading event to 'VimEnter'
+	config = function() -- This is the function that runs, AFTER loading
+		require("which-key").setup()
+
+		-- Document existing key chains
+		require("which-key").register({
+			["<leader>a"] = { name = "H[a]rpoon Add", _ = "which_key_ignore" },
+			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+			["<leader>i"] = { name = "[I]nlay", _ = "which_key_ignore" },
+			["<leader>t"] = { name = "[T]ab", _ = "which_key_ignore" },
+			["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+		})
+		-- visual mode
+		require("which-key").register({
+			["<leader>h"] = { "Git [H]unk" },
+		}, { mode = "v" })
+	end,
+}

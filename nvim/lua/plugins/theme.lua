1054c8cb731d3c4dec574bac15d061b05e945718
Nick Felicelli <nicholas.felicelli@cchmc.org>
2025-08-21 13:29:18 -0400

    Uptating theme and status line to change on light/dark

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -5,15 +5,29 @@ return {
     priority = 10010,
     init = function()
       vim.cmd([[
-				au User LumenLight echom 'tokyonight-day'
-				au User LumenDark echom 'tokyonight-night'
+				au User LumenLight echom 'catppucin-latte'
+				au User LumenDark echom 'catppuccin-mocha'
 			]])
+    end,
+  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 10000 },
+  {
+    "akinsho/bufferline.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if (vim.g.colors_name or ""):find("catppuccin") then
+        opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+      end
     end,
   },
   {
     "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight-night",
-    },
+    opts = function(_, opts)
+      if vim.o.background == "light" then
+        opts.colorscheme = "catppuccin-latte"
+      else
+        opts.colorscheme = "catppuccin-mocha"
+      end
+    end,
   },
 }
